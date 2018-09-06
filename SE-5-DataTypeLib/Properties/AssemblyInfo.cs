612fd56b6e3fb16286ee63c89db1c039e84126b8
davici <Davici@live.nl>
2018-09-06 15:11:49 +0200

    initial commit

---
+++ SE-5-DataTypeLib/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SE-5-DataTypeLib")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SE-5-DataTypeLib")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1957b85c-0d58-4844-ac61-85e5d6e53974")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SE-5-DataTypeLib/SEDataTypes.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SE5DataTypes
+{
+    class BinearyTree
+    {
+        public BinearyNode Root { get; }
+
+        public bool Add(Type item)
+        {
+            return false;
+        }
+
+        public BinearyTree(Type item)
+        {
+            Root = new BinearyNode(item);
+        }
+
+    }
+    class Node
+    {
+        public Type Data { get; set; }
+    }
+
+    class BinearyNode : Node
+    {
+
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+
+        public BinearyNode(Type item)
+        {
+            Data = item;
+        }
+    }
+}
